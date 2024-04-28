35a128e0ffe810bb17a9ba27c720d30a153d8cbb
AishaAgarwal <aishaagarwal19567@gmail.com>
2024-04-28 21:57:48 +0530

    refractor: improving streamlit UI

--- app.py
+++ app.py
@@ -1,5 +1,12 @@
 import spacy
 import streamlit as st
+import random 
+
+def generate_random_color():
+    r = random.randint(0, 255)
+    g = random.randint(0, 255)
+    b = random.randint(0, 255)
+    return f"rgb({r}, {g}, {b})"
 
 def main():
     # Load the trained model
@@ -18,16 +25,24 @@ def main():
         # Process the text and display the modified text with labeled entities
         modified_text = annotate_entities(example_text, doc)
 
-        # Display the modified text
-        st.write(modified_text)
+        # Display the modified text with different background boxes for each entity
+        st.markdown(modified_text, unsafe_allow_html=True)
 
 def annotate_entities(text, doc):
     modified_text = text
+    entity_colors = {}
+
 
     for ent in doc.ents:
         entity_text = ent.text
         entity_label = ent.label_
-        highlighted_text = f"**{entity_text} ({entity_label})**"
+
+         # Generate a random color for the entity label if it doesn't exist
+        if entity_label not in entity_colors:
+            entity_colors[entity_label] = generate_random_color()
+
+        # Apply background color and bold font to the entity
+        highlighted_text = f"<span style='background-color: {entity_colors[entity_label]}; font-weight: bold;'>{entity_text} ({entity_label})</span>"
         modified_text = modified_text.replace(entity_text, highlighted_text)
 
     return modified_text
@@ -36,5 +51,3 @@ if __name__ == "__main__":
     main()
 
 
-#cd "./OneDrive/Desktop/Atomic Asher/NER"
-#C:\Users\Shruti\AppData\Local\Programs\Python\Python39\python.exe -m streamlit run app.py
